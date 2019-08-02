c0cca557a956d8530c1bed2fa612523232888c51
Durgadevi-V <53587419+Durgadevi-V@users.noreply.github.com>
2019-08-02 00:08:01 -0900

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,6 +1,29 @@
 x=int(input("Enter the number:"))
 if x%4==0:
-    print("leap year")
+    if x%100==0:
+        if x%400==0:
+            print("leap year")
+        else:
+            print("not a leap year")
+    else:
+        print("leap year")
 else:
-    print("Not a leap year")
+    print("not a leap year")
       
+x=int(input("Enter the number:"))
+2
+if x%4==0:
+3
+    print("leap year")
+4
+else:
+5
+    print("Not a leap year")x=int(input("Enter the number:"))
+2
+if x%4==0:
+3
+    print("leap year")
+4
+else:
+5
+    print("Not a leap year")
