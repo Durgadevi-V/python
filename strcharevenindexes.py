4c2bf87f6f0281061c2f9d40ca69a8c7b86e52fd
Durgadevi-V <53587419+Durgadevi-V@users.noreply.github.com>
2019-08-09 03:06:55 -0400

    Delete strcharevenindexes.py

--- strcharevenindexes.py
+++
@@ -1,9 +0,0 @@
-def string(str):
-  result = "" 
-  for i in range(len(str)):
-    if i % 2 == 0:
-      result = result + str[i]
-  return result
-
-print(string('hariharan'))
-print(string('rakesh'))
